7708f9e0966926dc8c095a91aa12e20ecca94531
lyold <r.almeidagustavo@gmail.com>
2020-03-31 00:03:58 -0300

    Primeiro commit

---
+++ docs/scripts_carga.sql
@@ -0,0 +1,7 @@
+-- CARGA DE DADOS DE USUÁRIOS
+INSERT INTO Usuarios(Login, ChaveDeAcesso) values ('teste', 'teste');
+
+-- CARGA DE DADOS DE USUÁRIOS
+INSERT INTO Personalizacoes(TipoPersonalizacao, TempoAdicionalPreparo, CustoAdicional) values (0, 0, 3);
+INSERT INTO Personalizacoes(TipoPersonalizacao, TempoAdicionalPreparo, CustoAdicional) values (1, 0, 0);
+INSERT INTO Personalizacoes(TipoPersonalizacao, TempoAdicionalPreparo, CustoAdicional) values (2, 5, 5);
